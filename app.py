2d006371f04cecc25e3601b8b63a1467eb13da8c
Ben Kuhn <bn.kuhn1@gmail.com>
2024-05-16 20:56:37 -0700

    Add a voice recorder to the app

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from openai import OpenAI
 import streamlit as st
 from prompts.system_prompts import SYSTEM_PROMPT_1_A
+from st_audiorec import st_audiorec
 
 st.title("Pensieve")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4o"
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
-        {"role": "assistant", "content": "Tell me about your wedding day."}
+        {"role": "system", "content": SYSTEM_PROMPT_1_A},
+        {"role": "assistant", "content": "Tell me about your wedding day."},
     ]
 
-for message in st.session_state.messages:
+for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -27,7 +29,6 @@ if prompt := st.chat_input("What is up?"):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
             messages=[
-                {"role": "system", "content": SYSTEM_PROMPT_1_A},
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
@@ -35,3 +36,11 @@ if prompt := st.chat_input("What is up?"):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+with st.container():
+
+    wav_audio_data = st_audiorec()
+
+    if wav_audio_data is not None:
+        st.audio(wav_audio_data, format="audio/wav")
